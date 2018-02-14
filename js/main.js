5c22312cc20480ebcc1e2f1f76f9b8b08a282ee0
Bobby Caya <bob.caya1@gmail.com>
2018-02-14 08:08:52 -0700

    adds logic for game and images for buttons

--- js/main.js
+++ js/main.js
@@ -1,20 +1,80 @@
 function startGame(e) {
   var choice = e.target.id
   console.log(choice)
-  computerChoice = ['Rock', 'Paper', 'Scissors'][Math.floor(Math.random() * 3)]
-   console.log(computerChoice)
-   return computerChoice
+  compChoice = Math.random()
+  if (compChoice <0.34){
+    compChoice = "rock";
+  }
+  else if(compChoice <=0.67){
+    compChoice = "paper";
+  }
+  else{
+    compChoice = "scissors"
+    
+  }
+  console.log(compChoice)
+  checkWin(choice, compChoice)
 }
 
-function checkWin(checkWin){
-  if choice == 
+
+function checkWin(choice, compChoice){
+ if (choice === compChoice) {
+   return ("its a draw!")
+   
+   
+   }
+  if (choice === "rock"){
+    if(compChoice === "scissors"){
+      
+      console.log("rock wins")
+      
+    }
+  else{
+    
+    console.log("paper wins!")
+     }
+    }
+    if(choice ==="paper"){
+      if(compChoice === "rock"){
+        console.log("paper wins!")
+       
+        
+      }
+    else{
+      console.log("scissors win!")
+      
+      
+    }
+    }
+    if(choice === "scissors"){
+      if (compChoice === "paper"){
+        console.log("scissors win!")
+        
+      }
+    else{
+      console.log("rock wins!")
+      
+    }
+    }
+  }
+
+ 
+function results(checkWin){
+  if(checkWin === "its a draw!"){
+    function drawsCounter(){
+      return drawsCounter.count++
+    }
+    
+    console.log(`chicken score ${drawsCounter}`)
+    return drawsCounter
+  }
+
+  else if(checkWin === "user 1") {
+    ++user_score 
+    console.log(`cow score ${user_score}`)
+  }
 
 }
-
-function results (){
-
-}
-
 
 var choices = document.getElementsByClassName('choice');
 for (var i in choices) {
