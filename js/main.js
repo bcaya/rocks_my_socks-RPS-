2cadf8566bbf35a06ccf67573901a15965dd1292
Bobby Caya <bob.caya1@gmail.com>
2018-02-13 17:27:49 -0700

    Adds computer choice

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 function startGame(e) {
   var choice = e.target.id
   console.log(choice)
+  computerChoice = ['Rock', 'Paper', 'Scissors'][Math.floor(Math.random() * 3)]
+   console.log(computerChoice)
+   return computerChoice
 }
 
-var choices = document.getElementsByClassName('choice')
+function checkWin(checkWin){
+  if choice == 
+
+}
+
+function results (){
+
+}
+
+
+var choices = document.getElementsByClassName('choice');
 for (var i in choices) {
   try {
     choices[i].addEventListener('click', startGame)
+
   } catch(err) {
     //First load
   }
